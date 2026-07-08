4a5972fa301ed10fa39e6dcd06015b71a79b9233
Youssef Mohamed <yassooff2003@gmail.com>
2026-07-09 00:25:15 +0300

    make query pool work with transactions

--- flash-sale-ticketing/src/db/pool.ts
+++ flash-sale-ticketing/src/db/pool.ts
@@ -1,4 +1,4 @@
-import { Pool, QueryResult,QueryResultRow } from 'pg'
+import { Pool, QueryResult,QueryResultRow,PoolClient } from 'pg'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -13,4 +13,21 @@ export const query = <T extends QueryResultRow>(
   params?: any[]
 ): Promise<QueryResult<T>> => {
   return pool.query(text, params)
+}
+
+export const withTransaction = async <T>(
+  callback: (client: PoolClient) => Promise<T>
+): Promise<T> => {
+  const client = await pool.connect()
+  try {
+    await client.query('BEGIN')
+    const result = await callback(client)
+    await client.query('COMMIT')
+    return result
+  } catch (err) {
+    await client.query('ROLLBACK')
+    throw err
+  } finally {
+    client.release()
+  }
 }
