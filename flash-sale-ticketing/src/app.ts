169c73c3305a52893590a286ece69affe88ce11b
Youssef Mohamed <yassooff2003@gmail.com>
2026-07-14 21:33:33 +0300

    Add Redis expiry listener initialization in app setup

--- flash-sale-ticketing/src/app.ts
+++ flash-sale-ticketing/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import ticketRoutes from './routes/tickets';
+import { startExpiryListener } from './redis/expiryListener';
 
+// Start the Redis expiry listener
+startExpiryListener();
 dotenv.config();
 
 const app = express();
