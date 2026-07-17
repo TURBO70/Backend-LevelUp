a9348504153a31e8d1fe806c17b4d18d633875c5
Youssef Mohamed <yassooff2003@gmail.com>
2026-07-17 19:28:35 +0300

    Refactor server initialization: switch to using http server for WebSocket support

--- flash-sale-ticketing/src/app.ts
+++ flash-sale-ticketing/src/app.ts
@@ -1,20 +1,27 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import http from 'http'
+
 import ticketRoutes from './routes/tickets';
 import { startExpiryListener } from './redis/expiryListener';
 
 import { connectRabbitMQ } from './rabbitmq/client'
 import { startWorkers } from './workers/bookingWorkers'
+import { initWebSocketServer } from './websocket/server'
+import path from 'path'
+
+
 
 // startExpiryListener();
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app)
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
-// Routes
+app.use(express.static(path.join(__dirname, "../public")));// Routes
 app.use('/api/tickets', ticketRoutes);
 
 const start = async () => {
@@ -22,8 +29,10 @@ const start = async () => {
   await connectRabbitMQ()
   startWorkers()
   startExpiryListener()
+  initWebSocketServer(server)
 
-  app.listen(PORT, () => {
+
+  server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
 }
